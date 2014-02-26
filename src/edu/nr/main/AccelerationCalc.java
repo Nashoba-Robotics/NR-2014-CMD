148c8107173f8ff7363d1368170f67d30b2d8344
Alex Brinister <alex_brinister@yahoo.com>
2014-02-25 22:52:45 -0500

    started positioning based on acceleration. calibration routine written

--- src/edu/nr/main/AccelerationCalc.java
+++ src/edu/nr/main/AccelerationCalc.java
@@ -15,16 +15,36 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
  */
 public class AccelerationCalc 
 {
-    double lastTime = 0;
-    double lastAccelX, lastAccelY;
-    double initialVX = 0, initialVY;
-    public void updateAcceleration()
-    {
-        double deltaT = System.currentTimeMillis() - lastTime;
-        double vX = initialVX + (Robot.drive.getAccel(ADXL345_I2C.Axes.kX) * deltaT);
-        double vY = initialVY + (Robot.drive.getAccel(ADXL345_I2C.Axes.kY) * deltaT);
-        SmartDashboard.putNumber("X Velocity", vX);
-        SmartDashboard.putNumber("Y ", vY);
-        lastTime = System.currentTimeMillis();
+    private double sample_x, sample_y, sample_z;
+    private char count_x, count_y;
+    private int[] acc_x = new int[2];
+    private int[] acc_y = new int[2];
+    private int[] acc_z = new int[2];
+    private int[] velocity_x = new int[2];
+    private int[] velocity_y = new int[2];
+    private long[] position_x = new long[2];
+    private long[] position_y = new long[2];
+    private long[] position_z = new long[2];
+    private char direction;
+    private long s_state_x, s_state_y;
+    
+    public void run() {
+        
+    }
+    
+    private void calibrate() {
+        int count = 0;
+        
+        do {
+            sample_x = Robot.drive.getAccel(ADXL345_I2C.Axes.kX);
+            sample_y = Robot.drive.getAccel(ADXL345_I2C.Axes.kY);
+            sample_z = Robot.drive.getAccel(ADXL345_I2C.Axes.kZ);
+            s_state_x += sample_x;
+            s_state_y += sample_y;
+            count++;
+        } while(count != 1024);
+        
+        s_state_x = s_state_x >> 10;
+        s_state_y = s_state_x >> 10;
     }
 }
