973b4478afe26805bc262cab955a2fecfbe3eede
Alex Brinister <alex_brinister@yahoo.com>
2014-03-02 10:36:52 -0500

    Logitech done

--- src/edu/nr/main/oi/Controller.java
+++ src/edu/nr/main/oi/Controller.java
@@ -2,17 +2,25 @@ package edu.nr.main.oi;
 
 import edu.wpi.first.wpilibj.Joystick;
 
-public abstract class Controller extends Joystick
+public class Controller extends Joystick
 {
     public Controller(int port) {
         super(port);
     }
-    public static abstract class ControllerButtonType {}
+    public static class ControllerButtonType {}
 
-    public static abstract class ControllerAxisType {}
+    public static class ControllerAxisType {}
 
-    public abstract boolean getControllerRawButton(int button);
-    public abstract boolean getControllerButton(ControllerButtonType button);
-    public abstract double getControllerRawAxis(int axis);
-    public abstract double getControllerAxis(ControllerAxisType axis);
+    public boolean getControllerRawButton(int button) {
+        return false;
+    }
+    public boolean getControllerButton(ControllerButtonType button) {
+        return false;
+    }
+    public double getControllerRawAxis(int axis) {
+        return 0.0;
+    }
+    public double getControllerAxis(ControllerAxisType axis) {
+        return 0.0;
+    }
 }
