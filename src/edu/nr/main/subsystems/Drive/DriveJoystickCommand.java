c23efbd8189d0606e95acb7d67b02c5fd19485fb
Colin Willoughby <colinw@savi.net>
2014-03-04 23:39:24 -0500

    Added FieldCentric Class and NetworkTable

--- src/edu/nr/main/subsystems/Drive/DriveJoystickCommand.java
+++ src/edu/nr/main/subsystems/Drive/DriveJoystickCommand.java
@@ -22,13 +22,9 @@ public class DriveJoystickCommand extends Command
         this.requires(Robot.drive);
     }
     
-    double x = 0, y = 0, lastEncoderDistance = 0;
     protected void initialize() 
     {
-        x = 0;
-        y = 0;
-        lastEncoderDistance = 0;
-        Robot.drive.resetGyro();
+        
     }
     
     protected void execute()
@@ -36,24 +32,6 @@ public class DriveJoystickCommand extends Command
         double ySpeed = OI.getJoy1Y();
         double zSpeed = OI.getJoy1Z();
         
-        double angle = Robot.drive.getGyroAngle();
-        angle = angle * (Math.PI / 180);
-        
-        double ave = Robot.drive.getAverageEncoderDistance();// * (34d/33d);
-        double delta_x_r = (ave-lastEncoderDistance);
-        double deltax = delta_x_r * Math.cos(-angle);
-        double deltay = delta_x_r * Math.sin(-angle);
-        x += deltax;
-        y += deltay;
-        
-        lastEncoderDistance = ave;
-        
-        SmartDashboard.putNumber("Location x", x);
-        SmartDashboard.putNumber("Location y", y);
-        
-        
-        //SmartDashboard.putNumber("Y axis", ySpeed);
-        //SmartDashboard.putNumber("Z axis", zSpeed);
         SmartDashboard.putNumber("Encoder 1", Robot.drive.getRawEncoder(1));
         SmartDashboard.putNumber("Encoder 2", Robot.drive.getRawEncoder(2));
         
@@ -64,8 +42,8 @@ public class DriveJoystickCommand extends Command
             zSpeed = 0;
         
         //Cut the speeds down because they are really fast
-        zSpeed = (zSpeed/3) * 2;
-        ySpeed = (ySpeed/4) * 3;
+        zSpeed = zSpeed * 2 / 3;
+        ySpeed = ySpeed * 3 / 4;
         
         Robot.drive.drive(ySpeed, zSpeed);
     }
